fb742f0f113c2e1bb50bd45ecaa99d8cc2d5463c
Thaïs Brun <travailthais@outlook.fr>
2024-01-15 09:04:51 +0100

    TP6 en cours

---
+++ app/m05tp1/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    //passage de paramètres entre fragment
+    id("androidx.navigation.safeargs.kotlin")
+    //parcelable
+    id("kotlin-parcelize")
+}
+
+android {
+    namespace = "com.example.m05tp1"
+    compileSdk = 34
+
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
+    defaultConfig {
+        applicationId = "com.example.m05tp1"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
---
+++ app/m05tp1/src/main/java/com/example/m05tp1/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.m05tp1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.m05tp1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
